e6e3c4dab760c4365f3549d3ba1aaa11b86bee41
Chandler Escude <chandler.escude@dreambox.com>
2023-02-14 09:59:45 -0500

    Build format changes to work with latest nightly

--- build.zig
+++ build.zig
@@ -44,13 +44,7 @@ fn configCSources(step: *std.build.LibExeObjStep) void {
     step.linkLibC();
 }
 
-const zopts = std.build.Pkg{
-    .name = "zopts",
-    .source = .{ .path = "extern/zopts/src/zopts.zig" },
-    .dependencies = &[_]std.build.Pkg{},
-};
-
-pub fn build(b: *std.build.Builder) void {
+pub fn build(b: *std.Build) void {
     // Standard target options allows the person running `zig build` to choose
     // what target to build for. Here we do not override the defaults, which
     // means any target is allowed, and the default is native. Other options
@@ -59,13 +53,18 @@ pub fn build(b: *std.build.Builder) void {
 
     // Standard release options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
-    const mode = b.standardReleaseOptions();
+    const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable("simsim", "src/main.zig");
+    const exe = b.addExecutable(.{
+        .name = "simsim",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
     configCSources(exe);
-    exe.addPackage(zopts);
-    exe.setTarget(target);
-    exe.setBuildMode(mode);
+    exe.addModule("zopts", b.createModule(.{ .source_file = .{
+        .path = "extern/zopts/src/zopts.zig",
+    } }));
     exe.install();
 
     const run_cmd = exe.run();
@@ -77,10 +76,12 @@ pub fn build(b: *std.build.Builder) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    const exe_tests = b.addTest("src/main.zig");
+    const exe_tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
     configCSources(exe_tests);
-    exe_tests.setTarget(target);
-    exe_tests.setBuildMode(mode);
 
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&exe_tests.step);
